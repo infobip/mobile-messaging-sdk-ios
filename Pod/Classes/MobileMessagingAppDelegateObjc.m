4d01544617a937802a6783f853a24471a531f553
Andrey K <kaskaaddnb@gmail.com>
2016-05-26 13:07:17 +0300

    Update: Force to override methods in subclass rigth way.

--- Pod/Classes/MobileMessagingAppDelegateObjc.m
+++ Pod/Classes/MobileMessagingAppDelegateObjc.m
@@ -11,20 +11,17 @@
 
 @implementation MobileMessagingAppDelegateObjc
 -(NSString *)applicationCode {
-	NSAssert(NO, @"Application code not set. Please override `applicationCode` variable in your subclass of `MobileMessagingAppDelegate`.");
+	[NSException raise:NSInternalInconsistencyException format:@"Application code not set. Please override `applicationCode` variable in your subclass of `MobileMessagingAppDelegate`."];
     return nil;
 }
 
 -(UIUserNotificationType)userNotificationType {
-	NSAssert(NO, @"UserNotificationType not set. Please override `userNotificationType` variable in your subclass of `MobileMessagingAppDelegate`.");
+	[NSException raise:NSInternalInconsistencyException format:@"UserNotificationType not set. Please override `userNotificationType` variable in your subclass of `MobileMessagingAppDelegate`."];
     return UIUserNotificationTypeNone;
 }
 
 -(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	[MobileMessaging startWithApplicationCode:self.applicationCode];
-	UIUserNotificationSettings * settings = [UIUserNotificationSettings settingsForTypes:self.userNotificationType categories:nil];
-	[application registerUserNotificationSettings:settings];
-	[application registerForRemoteNotifications];
+	[MobileMessaging startWithApplicationCode: self.userNotificationType applicationCode: self.applicationCode];
 	return [self mm_application:application didFinishLaunchingWithOptions:launchOptions];
 }
 -(BOOL)mm_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
