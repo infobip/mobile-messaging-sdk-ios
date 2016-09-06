7b537e7d9b8c0727f95c283b072b2bc752d039c8
okoroleva <olga.koroleva@infobip.com>
2016-09-06 17:35:44 +0300

    NSDictionary returned back

--- Pod/Classes/Utils/MMUtils.swift
+++ Pod/Classes/Utils/MMUtils.swift
@@ -38,22 +38,32 @@ struct DateStaticFormatters {
 
 public extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
 	public var mm_apsAlertBody: String? {
-		var messageDict: [AnyHashable: Any]
-		if let aps = self["aps"] as? [AnyHashable: Any] {
+		return (self as NSDictionary).mm_apsAlertBody
+	}
+	
+	public var mm_messageId: String? {
+		return (self as NSDictionary).mm_messageId
+	}
+}
+
+public extension NSDictionary {
+	public var mm_apsAlertBody: String? {
+		var messageDict: NSDictionary
+		if let aps = self["aps"] as? NSDictionary {
 			messageDict = aps
 		} else {
 			messageDict = self
 		}
-
+		
 		if let alert = messageDict["alert"] as? String {
 			return alert
-		} else if let alert = messageDict["alert"] as? [AnyHashable: Any], let body = alert["body"] as? String {
+		} else if let alert = messageDict["alert"] as? NSDictionary, let body = alert["body"] as? String {
 			return body
 		} else {
 			return nil
 		}
 	}
-
+	
 	public var mm_messageId: String? {
 		return self["messageId"] as? String
 	}
