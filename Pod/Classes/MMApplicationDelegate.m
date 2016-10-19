39368caae395dc7ef44cb57893f7d469444b6ff6
okoroleva <olga.koroleva@infobip.com>
2016-10-19 17:21:24 +0300

    gtomasic-MM-1065-cordova-support

---
+++ Pod/Classes/MMApplicationDelegate.m
@@ -0,0 +1,83 @@
+//
+// Created by Goran Tomasic on 10/10/2016.
+//
+
+#import "MMApplicationDelegate.h"
+#import <MobileMessaging/MobileMessaging-Swift.h>
+
+@interface MMApplicationDelegate() {
+	id<UIApplicationDelegate> _applicationDelegate;
+	UIBackgroundTaskIdentifier _backgroundTask;
+}
+@end
+
+@implementation MMApplicationDelegate
+
++ (instancetype)sharedInstaller {
+	static MMApplicationDelegate *_sharedInstaller = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		_sharedInstaller = [[self alloc] init];
+	});
+	
+	return _sharedInstaller;
+}
+
++ (void)install {
+	[[self sharedInstaller] install];
+}
+
+- (void)install {
+	if (!self.installed){
+		_applicationDelegate = [UIApplication sharedApplication].delegate;
+		UIResponder *responder = (UIResponder *) _applicationDelegate;
+		self.window = [responder valueForKey:@"window"];
+		[[UIApplication sharedApplication] setDelegate:self];
+		
+		NSLog(@"***** AppDelegate was intercepted");
+		
+		_backgroundTask = UIBackgroundTaskInvalid;
+		_installed = YES;
+	}
+}
+
+#pragma mark - Application Delegate Methods
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+	[MobileMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+	if (_applicationDelegate && [_applicationDelegate respondsToSelector:@selector(application:didReceiveRemoteNotification:fetchCompletionHandler:)]) {
+		[_applicationDelegate application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+	}
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+	[MobileMessaging didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+	if (_applicationDelegate && [_applicationDelegate respondsToSelector:@selector(application:didRegisterForRemoteNotificationsWithDeviceToken:)]) {
+		[_applicationDelegate application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+	}
+}
+
+#pragma mark - Unknown method handlers
+
+// These methods likely won't be needed, but are added here so any Obj-C calls that are aimed at the original ApplicationDelegate will be forwarded
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+	NSObject *appDelegateObject = (NSObject *)_applicationDelegate;
+	if ([super respondsToSelector:[anInvocation selector]]) {
+		[super forwardInvocation:anInvocation];
+	} else if (appDelegateObject && [appDelegateObject respondsToSelector:[anInvocation selector]]){
+		[anInvocation invokeWithTarget:appDelegateObject];
+	}
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+	NSMethodSignature *methodSignature = [super methodSignatureForSelector:aSelector];
+	
+	if (!methodSignature){
+		NSObject *appDelegateObject = (NSObject *)_applicationDelegate;
+		methodSignature = [appDelegateObject methodSignatureForSelector:aSelector];
+	}
+	
+	return methodSignature;
+}
+
+@end
