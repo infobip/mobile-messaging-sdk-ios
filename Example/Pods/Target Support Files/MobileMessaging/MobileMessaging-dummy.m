41c145a54bdcb2d250eb10b08d86b1dd215cbf56
Andrey K <kaskaaddnb@gmail.com>
2017-09-22 18:01:56 +0300

    cocoapods gitignore reset

---
+++ Example/Pods/Target Support Files/MobileMessaging/MobileMessaging-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MobileMessaging : NSObject
+@end
+@implementation PodsDummy_MobileMessaging
+@end
---
+++ Example/Pods/Target Support Files/Pods-MobileMessagingExample_Tests_Device/Pods-MobileMessagingExample_Tests_Device-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileMessagingExample_Tests_Device : NSObject
+@end
+@implementation PodsDummy_Pods_MobileMessagingExample_Tests_Device
+@end
