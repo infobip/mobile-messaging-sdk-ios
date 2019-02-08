d2e5d3ea3ae30ba99ede5e703ca8b8fbee36adb6
Andrey K <kaskaaddnb@gmail.com>
2019-02-08 15:34:45 +0300

    Fixes caching for user/installation/internal data.

--- Classes/Core/InternalStorage/Archivable.swift
+++ Classes/Core/InternalStorage/Archivable.swift
@@ -17,6 +17,7 @@ protocol Archivable: ArchivableCurrent {
 	static func unarchiveDirty() -> Self
 	func handleDirtyChanges(old: Self, new: Self)
 	static func modifyAll(with block: (Self) -> Void)
+	static func modifyDirty(with block: (Self) -> Void)
 }
 
 protocol ArchivableCurrent {
@@ -29,40 +30,54 @@ protocol ArchivableCurrent {
 	func removeSensitiveData()
 	static func resetCurrent()
 	static func unarchiveCurrent() -> Self
-	static func remove(at path: String)
+	static func removeArchive(at path: String)
 	func handleCurrentChanges(old: Self, new: Self)
+	static func modifyCurrent(with block: (Self) -> Void)
 }
 
 extension ArchivableCurrent where Self: NSCopying {
 	func archiveCurrent() {
 		let old = Self.unarchiveCurrent()
+		Self.cached.set(value: self.copy() as? Self, forKey: Self.currentPath)
 		archive(at: Self.currentPath)
 		handleCurrentChanges(old: old, new: self)
 	}
 	func archive(at path: String) {
-		Self.cached.set(value: self.copy() as? Self, forKey: path)
+		MMLogDebug("Archiving \(self) at \(path)")
 		let save = self.copy() as! Self
 		save.removeSensitiveData()
 		NSKeyedArchiver.archiveRootObject(save, toFile: path)
 	}
 	static func resetCurrent() {
-		Self.remove(at: Self.currentPath)
+		Self.cached.set(value: nil, forKey: Self.currentPath)
+		Self.removeArchive(at: Self.currentPath)
 	}
 	static func unarchiveCurrent() -> Self {
-		return Self.unarchive(from: Self.currentPath) ?? Self.empty
-	}
-	static func unarchive(from path: String) -> Self? {
-		if let cached = Self.cached.getValue(forKey: path) {
-			return cached.copy() as? Self
+		if let cached = Self.cached.getValue(forKey: Self.currentPath), let ret = cached.copy() as? Self {
+			return ret
 		} else {
-			let newVal = (NSKeyedUnarchiver.unarchiveObject(withFile: path) as? Self)
-			Self.cached.set(value: newVal?.copy() as? Self, forKey: path)
-			return newVal
+			let current = Self.unarchive(from: Self.currentPath) ?? Self.empty
+			Self.cached.set(value: current.copy() as? Self, forKey: Self.currentPath)
+			return current
 		}
 	}
-	static func remove(at path: String) {
-		Self.cached.set(value: nil, forKey: path)
-		try? FileManager.default.removeItem(atPath: path)
+	static func unarchive(from path: String) -> Self? {
+		let unarchived = NSKeyedUnarchiver.unarchiveObject(withFile: path) as? Self
+		MMLogDebug("Unarchived \(String(describing: unarchived)) from \(path)")
+		return unarchived
+	}
+	static func removeArchive(at path: String) {
+		MMLogDebug("Removing archive at \(path)")
+		do {
+			try FileManager.default.removeItem(atPath: path)
+		} catch {
+			MMLogDebug("Unexpected error while removing archive at \(path): \(error)")
+		}
+	}
+	static func modifyCurrent(with block: (Self) -> Void) {
+		let o = Self.unarchiveCurrent()
+		block(o)
+		o.archiveCurrent()
 	}
 }
 
@@ -75,6 +90,7 @@ extension Archivable where Self: NSCopying {
 		if var copy = self.copy() as? Self {
 			copy.version = version + 1
 			let old = Self.unarchiveDirty()
+			Self.cached.set(value: self.copy() as? Self, forKey: Self.dirtyPath)
 			copy.archive(at: Self.dirtyPath)
 			handleDirtyChanges(old: old, new: self)
 		}
@@ -84,18 +100,25 @@ extension Archivable where Self: NSCopying {
 		Self.resetCurrent()
 	}
 	static func resetDirty() {
-		Self.remove(at: Self.dirtyPath)
+		Self.cached.set(value: nil, forKey: Self.dirtyPath)
+		Self.removeArchive(at: Self.dirtyPath)
 	}
 	static func unarchiveDirty() -> Self {
-		return Self.unarchive(from: Self.dirtyPath) ?? Self.unarchiveCurrent()
+		if let cached = Self.cached.getValue(forKey: Self.dirtyPath), let ret = cached.copy() as? Self {
+			return ret
+		} else {
+			let dirty = Self.unarchive(from: Self.dirtyPath) ?? Self.unarchiveCurrent()
+			Self.cached.set(value: dirty.copy() as? Self, forKey: Self.dirtyPath)
+			return dirty
+		}
 	}
 	static func modifyAll(with block: (Self) -> Void) {
+		Self.modifyDirty(with: block)
+		Self.modifyCurrent(with: block)
+	}
+	static func modifyDirty(with block: (Self) -> Void) {
 		let du = Self.unarchiveDirty()
 		block(du)
 		du.archiveDirty()
-
-		let cu = Self.unarchiveCurrent()
-		block(cu)
-		cu.archiveCurrent()
 	}
 }
