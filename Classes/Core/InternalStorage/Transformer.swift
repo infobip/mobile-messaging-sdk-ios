0a2ef5d7b19ab9ca0fc97db44dea56ac0cee3e18
Andrey K <kaskaaddnb@gmail.com>
2019-10-10 12:02:11 +0300

    Aligns transformable datatypes with new iOS 13 requirements

---
+++ Classes/Core/InternalStorage/Transformer.swift
@@ -0,0 +1,108 @@
+//
+//  Transformer.swift
+//  MobileMessaging
+//
+//  Created by Andrey Kadochnikov on 09.10.2019.
+//
+
+import Foundation
+
+@objc(DefaultTransformer)
+class DefaultTransformer: ValueTransformer {
+	override class func transformedValueClass() -> AnyClass {
+		return NSData.self
+	}
+
+	override open func reverseTransformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? Data else {
+			return nil
+		}
+		return NSKeyedUnarchiver.unarchiveObject(with: value)
+	}
+
+	override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let value = value else {
+			return nil
+		}
+		return NSKeyedArchiver.archivedData(withRootObject: value)
+	}
+}
+
+@objc(EmailTransformer)
+class EmailTransformer: ValueTransformer {
+	override class func transformedValueClass() -> AnyClass {
+		return NSData.self
+	}
+
+	override open func reverseTransformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? Data else {
+			return nil
+		}
+		return NSKeyedUnarchiver.unarchiveObject(with: value)
+	}
+
+	override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? [Email] else {
+			return nil
+		}
+		return NSKeyedArchiver.archivedData(withRootObject: value)
+	}
+}
+
+@objc(InstallationTransformer)
+class InstallationTransformer: ValueTransformer {
+	override class func transformedValueClass() -> AnyClass {
+		return NSData.self
+	}
+
+	override open func reverseTransformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? Data else {
+			return nil
+		}
+		return NSKeyedUnarchiver.unarchiveObject(with: value)
+	}
+
+	override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? [Installation] else {
+			return nil
+		}
+		return NSKeyedArchiver.archivedData(withRootObject: value)
+	}
+}
+
+@objc(PhoneTransformer)
+class PhoneTransformer: ValueTransformer {
+	override class func transformedValueClass() -> AnyClass {
+		return NSData.self
+	}
+
+	override open func reverseTransformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? Data else {
+			return nil
+		}
+		return NSKeyedUnarchiver.unarchiveObject(with: value)
+	}
+
+	override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+
+	override func transformedValue(_ value: Any?) -> Any? {
+		guard let value = value as? [Phone] else {
+			return nil
+		}
+		return NSKeyedArchiver.archivedData(withRootObject: value)
+	}
+}
