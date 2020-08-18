f16d2662f2765579fa77f795a6ceaab587cee374
Olga Koroleva <Olga.Koroleva@infobip.com>
2020-08-18 13:32:51 +0000

    Pull request #134: Okoroleva MM-4078 Placeholder for appCode in Example
    Merge in MML/infobip-mobile-messaging-ios from okoroleva_MM-4078_ExampleFix to master
    
    Squashed commit of the following:
    
    commit d45bdfef5508be2eef8171a227a83bb92aead46b
    Author: Olga Koroleva <Olga.Koroleva@infobip.com>
    Date:   Tue Aug 18 16:20:02 2020 +0300
    
        placeholder for appCode
    
    commit 9d18d557f6c29dfb4009c334bad6f0e96c97edda
    Author: Olga Koroleva <Olga.Koroleva@infobip.com>
    Date:   Tue Aug 18 16:13:59 2020 +0300
    
        Revert "fix"
    
        This reverts commit 5d2754bc2ce0ede647fa5a0ad3d03f31aae1f222.
    
    commit 5d2754bc2ce0ede647fa5a0ad3d03f31aae1f222
    Author: Olga Koroleva <Olga.Koroleva@infobip.com>
    Date:   Tue Aug 18 15:57:08 2020 +0300
    
        fix

--- Example/MobileMessagingExample/AppDelegate.swift
+++ Example/MobileMessagingExample/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		if !ProcessInfo.processInfo.arguments.contains("-IsStartedToRunTests") {
 			setupLogging()
 			MobileMessaging
-			.withApplicationCode(
-				"3c59f6e341a6896fc05b8cd7e3f3fdf8-031a75db-fd8f-46b0-9f2b-a2e915d7b952_",
+            .withApplicationCode(
+                "<# your application code #>",
 				notificationType: UserNotificationType(options: [.alert, .sound]))?
 			.withInteractiveNotificationCategories(customCategories)
 			.start()
