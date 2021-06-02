1dfba4de6125634218b82364343413c43a74a719
Andrey Kadochnikov <Andrey.Kadochnikov@infobip.com>
2021-06-02 11:23:13 +0000

    Pull request #173: MM-4577 bugfix for session reporting
    Merge in MML/infobip-mobile-messaging-ios from akadochnikov-MM-4577-session_report_crash to master
    
    Squashed commit of the following:
    
    commit cb8e70fdf5909fae90636b5bf861c03aca50d889
    Author: Andrey K <kaskaaddnb@gmail.com>
    Date:   Wed Jun 2 12:05:23 2021 +0300
    
        no message
    
    commit 45ce1fd9b1f5365d0458099afbb4eec03e2c5a55
    Author: Andrey K <kaskaaddnb@gmail.com>
    Date:   Wed Jun 2 12:04:32 2021 +0300
    
        no message
    
    commit 56117a246926a876d822eb8555fd5bb63d5abae8
    Author: Andrey K <kaskaaddnb@gmail.com>
    Date:   Wed Jun 2 12:00:21 2021 +0300
    
        fix

--- Classes/UserSession/UserSessionMapper.swift
+++ Classes/UserSession/UserSessionMapper.swift
@@ -8,36 +8,36 @@
 import Foundation
 
 class UserSessionMapper {
-	class func requestPayload(newSessions: [UserSessionReportObject]?, finishedSessions: [UserSessionReportObject]?) -> RequestBody {
-		var ret = RequestBody()
-		if let newSessions = newSessions, !newSessions.isEmpty {
-			ret[Consts.UserSessions.sessionStarts] = newSessions.map { DateStaticFormatters.ISO8601SecondsFormatter.string(from: $0.startDate) }
-		}
-
-		if let finishedSessions = finishedSessions, !finishedSessions.isEmpty {
-			let deduplicatedFinishedSessions = distinct(
-				source: finishedSessions,
-				distinctAttribute: {$0.startDate})
-			{ (l, r) -> UserSessionReportObject in
-				return l.endDate.compare(r.endDate) == ComparisonResult.orderedDescending ? l : r
-			}
-
-			ret[Consts.UserSessions.sessionBounds] = Dictionary(uniqueKeysWithValues: deduplicatedFinishedSessions.map{
-				(DateStaticFormatters.ISO8601SecondsFormatter.string(from:$0.startDate),
-				 DateStaticFormatters.ISO8601SecondsFormatter.string(from:$0.endDate))
-			})
-		}
-		ret["systemData"] = MobileMessaging.userAgent.systemData.requestPayload
-		return ret
-	}
+    class func requestPayload(newSessions: [UserSessionReportObject]?, finishedSessions: [UserSessionReportObject]?) -> RequestBody {
+        var ret = RequestBody()
+        if let newSessions = newSessions, !newSessions.isEmpty {
+            ret[Consts.UserSessions.sessionStarts] = newSessions.map { DateStaticFormatters.ISO8601SecondsFormatter.string(from: $0.startDate) }
+        }
+        
+        if let finishedSessions = finishedSessions, !finishedSessions.isEmpty {
+            let deduplicatedFinishedSessions = distinct(
+                source: finishedSessions,
+                distinctAttribute: {DateStaticFormatters.ISO8601SecondsFormatter.string(from:$0.startDate)})
+            { (l, r) -> UserSessionReportObject in
+                return l.endDate.compare(r.endDate) == ComparisonResult.orderedDescending ? l : r
+            }
+            
+            ret[Consts.UserSessions.sessionBounds] = Dictionary(uniqueKeysWithValues: deduplicatedFinishedSessions.map{
+                (DateStaticFormatters.ISO8601SecondsFormatter.string(from:$0.startDate),
+                 DateStaticFormatters.ISO8601SecondsFormatter.string(from:$0.endDate))
+            })
+        }
+        ret["systemData"] = MobileMessaging.userAgent.systemData.requestPayload
+        return ret
+    }
 }
 
 struct DistinctWrapper <T, E : Hashable>: Hashable {
-	var underlyingObject: T
-	var distinctAttribute: E
-	func hash(into hasher: inout Hasher) {
-		hasher.combine(distinctAttribute)
-	}
+    var underlyingObject: T
+    var distinctAttribute: E
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(distinctAttribute)
+    }
 }
 /// Returns distinct collection, duplicates conflicts resolved by custom resolution implementation
 /// - parameter source: Collectiom that may contain duplicate objects
@@ -48,22 +48,22 @@ struct DistinctWrapper <T, E : Hashable>: Hashable {
 /// - parameter r: One of the conflicting source collection element
 /// - returns: Deduplicated source array
 func distinct<S : Sequence, T, E : Hashable>(source: S,
-											 distinctAttribute: (_ element: T) -> E,
-											 resolution: (_ l: T, _ r: T) -> T) -> [T]  where S.Element == T {
-	var added = Set<DistinctWrapper<T, E>>()
-	for next in source.map({
-		return DistinctWrapper(underlyingObject: $0, distinctAttribute: distinctAttribute($0))
-	}) {autoreleasepool{
-		if let indexOfExisting = added.firstIndex(of: next) {
-			let prev = added[indexOfExisting]
-			let winner = resolution(prev.underlyingObject, next.underlyingObject)
-			added.update(with: DistinctWrapper(underlyingObject: winner, distinctAttribute: distinctAttribute(winner)))
-		} else {
-			added.insert(next)
-		}
-		}}
-	return Array(added).map( { return $0.underlyingObject } )
+                                             distinctAttribute: (_ element: T) -> E,
+                                             resolution: (_ l: T, _ r: T) -> T) -> [T]  where S.Element == T {
+    var added = Set<DistinctWrapper<T, E>>()
+    for next in source.map({
+        return DistinctWrapper(underlyingObject: $0, distinctAttribute: distinctAttribute($0))
+    }) {autoreleasepool{
+        if let indexOfExisting = added.firstIndex(of: next) {
+            let prev = added[indexOfExisting]
+            let winner = resolution(prev.underlyingObject, next.underlyingObject)
+            added.update(with: DistinctWrapper(underlyingObject: winner, distinctAttribute: distinctAttribute(winner)))
+        } else {
+            added.insert(next)
+        }
+    }}
+    return Array(added).map( { return $0.underlyingObject } )
 }
 func == <T, E>(lhs: DistinctWrapper<T, E>, rhs: DistinctWrapper<T, E>) -> Bool {
-	return lhs.hashValue == rhs.hashValue
+    return lhs.hashValue == rhs.hashValue
 }
--- Example/Tests/MobileMessagingTests/UserSessionTests.swift
+++ Example/Tests/MobileMessagingTests/UserSessionTests.swift
@@ -10,159 +10,176 @@ import XCTest
 @testable import MobileMessaging
 
 class UserSessionTests: MMTestCase {
-
-	func testThatUserSessionDataPersisted() {
-		MMTestCase.cleanUpAndStop()
-		MMTestCase.startWithCorrectApplicationCode()
-		weak var expectation = self.expectation(description: "case is finished")
-		mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
-		let now = MobileMessaging.date.now.timeIntervalSince1970
-
-		// when
-		self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-			timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
-
-				self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-					timeTravel(to: Date(timeIntervalSince1970: now + 10), block: {
-
-						self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-							expectation?.fulfill()
-						}
-					})
-				}
-			})
-		}
-
-		// then
-		waitForExpectations(timeout: 20, handler: { _ in
-			let ctx = self.storage.mainThreadManagedObjectContext!
-			let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
-			XCTAssertEqual(sessions.count, 1) // one unreported session
-			XCTAssertEqual(sessions.first!.startDate.timeIntervalSince1970, now)
-			XCTAssertEqual(sessions.first!.endDate.timeIntervalSince1970, now + 10)
-		})
-	}
-
-	func testSuccessfulSessionReporting() {
-		MMTestCase.cleanUpAndStop()
-		MMTestCase.startWithCorrectApplicationCode()
-		weak var expectation = self.expectation(description: "case is finished")
-		mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
-
-		let remoteApiProvider = RemoteAPIProviderStub()
-		remoteApiProvider.sendUserSessionClosure = { _, _, _ in
-			return UserSessionSendingResult.Success(EmptyResponse())
-		}
-		mobileMessagingInstance.remoteApiProvider = remoteApiProvider
-		let now = MobileMessaging.date.now.timeIntervalSince1970
-
-		// when
-		self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-			timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
-
-				self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-					timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
-
-						self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
-							expectation?.fulfill()
-						}
-					})
-				}
-			})
-		}
-
-		// then
-		waitForExpectations(timeout: 20, handler: { _ in
-			let ctx = self.storage.mainThreadManagedObjectContext!
-			let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
-			XCTAssertEqual(sessions.count, 1) // one unreported session remains, one should be removed
-			XCTAssertEqual(sessions.first!.startDate.timeIntervalSince1970, now + 35)
-			XCTAssertEqual(sessions.first!.endDate.timeIntervalSince1970, now + 40)
-		})
-	}
-
-	func testFailedSessionReporting() {
-		MMTestCase.cleanUpAndStop()
-		MMTestCase.startWithCorrectApplicationCode()
-		weak var expectation = self.expectation(description: "case is finished")
-		mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
-
-		let remoteApiProvider = RemoteAPIProviderStub()
-		remoteApiProvider.sendUserSessionClosure = { _, _, _ in
-			return UserSessionSendingResult.Failure(nil)
-		}
-		mobileMessagingInstance.remoteApiProvider = remoteApiProvider
-		let now = MobileMessaging.date.now.timeIntervalSince1970
-
-		// when
-		self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-			timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
-
-				self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-					timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
-
-						self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
-							expectation?.fulfill()
-						}
-					})
-				}
-			})
-		}
-
-		// then
-		waitForExpectations(timeout: 20, handler: { _ in
-			let ctx = self.storage.mainThreadManagedObjectContext!
-			let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
-			XCTAssertEqual(sessions.count, 2)
-
-			XCTAssertNotNil(sessions.first { (o) -> Bool in
-				return (o.startDate.timeIntervalSince1970 == now + 35) && (o.endDate.timeIntervalSince1970 == now + 40)
-			})
-			XCTAssertNotNil(sessions.first { (o) -> Bool in
-				return (o.startDate.timeIntervalSince1970 == now) && (o.endDate.timeIntervalSince1970 == now + 5)
-			})
-
-		})
-	}
-
-	func testSessionReportDoesNotHaveSessionDuplicates() {
-			MMTestCase.cleanUpAndStop()
-			MMTestCase.startWithCorrectApplicationCode()
-			weak var expectation = self.expectation(description: "case is finished")
-
-			let remoteApiProvider = RemoteAPIProviderStub()
-			remoteApiProvider.sendUserSessionClosure = { _, _, _ in
-				return UserSessionSendingResult.Success(EmptyResponse())
-			}
-			mobileMessagingInstance.remoteApiProvider = remoteApiProvider
-			let now = MobileMessaging.date.now.timeIntervalSince1970
-
-			// when
-			mobileMessagingInstance.pushRegistrationId = "reg1"
-			self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-				self.mobileMessagingInstance.pushRegistrationId = "reg2"
-					self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
-
-						timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
-
-							self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
-								expectation?.fulfill()
-							}
-						})
-					}
-			}
-
-			// then
-			waitForExpectations(timeout: 20, handler: { _ in
-				let ctx = self.storage.mainThreadManagedObjectContext!
-				let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
-				XCTAssertEqual(sessions.count, 1) // one unreported session remains
-			})
-		}
+    
+    func testThatUserSessionDataPersisted() {
+        MMTestCase.cleanUpAndStop()
+        MMTestCase.startWithCorrectApplicationCode()
+        weak var expectation = self.expectation(description: "case is finished")
+        mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
+        let now = MobileMessaging.date.now.timeIntervalSince1970
+        
+        // when
+        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+            
+            timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
+                
+                self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+                    
+                    timeTravel(to: Date(timeIntervalSince1970: now + 10), block: {
+                        
+                        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+                            expectation?.fulfill()
+                        }
+                    })
+                }
+            })
+        }
+        
+        // then
+        waitForExpectations(timeout: 20, handler: { _ in
+            let ctx = self.storage.mainThreadManagedObjectContext!
+            let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
+            XCTAssertEqual(sessions.count, 1) // one unreported session
+            XCTAssertEqual(sessions.first!.startDate.timeIntervalSince1970, now)
+            XCTAssertEqual(sessions.first!.endDate.timeIntervalSince1970, now + 10)
+        })
+    }
+    
+    func testSuccessfulSessionReporting() {
+        MMTestCase.cleanUpAndStop()
+        MMTestCase.startWithCorrectApplicationCode()
+        weak var expectation = self.expectation(description: "case is finished")
+        mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
+        
+        let remoteApiProvider = RemoteAPIProviderStub()
+        remoteApiProvider.sendUserSessionClosure = { _, _, _ in
+            return UserSessionSendingResult.Success(EmptyResponse())
+        }
+        mobileMessagingInstance.remoteApiProvider = remoteApiProvider
+        let now = MobileMessaging.date.now.timeIntervalSince1970
+        
+        // when
+        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+            
+            timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
+                
+                self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+                    
+                    timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
+                        
+                        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
+                            expectation?.fulfill()
+                        }
+                    })
+                }
+            })
+        }
+        
+        // then
+        waitForExpectations(timeout: 20, handler: { _ in
+            let ctx = self.storage.mainThreadManagedObjectContext!
+            let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
+            XCTAssertEqual(sessions.count, 1) // one unreported session remains, one should be removed
+            XCTAssertEqual(sessions.first!.startDate.timeIntervalSince1970, now + 35)
+            XCTAssertEqual(sessions.first!.endDate.timeIntervalSince1970, now + 40)
+        })
+    }
+    
+    func testFailedSessionReporting() {
+        MMTestCase.cleanUpAndStop()
+        MMTestCase.startWithCorrectApplicationCode()
+        weak var expectation = self.expectation(description: "case is finished")
+        mobileMessagingInstance.pushRegistrationId = MMTestConstants.kTestCorrectInternalID
+        
+        let remoteApiProvider = RemoteAPIProviderStub()
+        remoteApiProvider.sendUserSessionClosure = { _, _, _ in
+            return UserSessionSendingResult.Failure(nil)
+        }
+        mobileMessagingInstance.remoteApiProvider = remoteApiProvider
+        let now = MobileMessaging.date.now.timeIntervalSince1970
+        
+        // when
+        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+            
+            timeTravel(to: Date(timeIntervalSince1970: now + 5), block: {
+                
+                self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+                    
+                    timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
+                        
+                        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
+                            expectation?.fulfill()
+                        }
+                    })
+                }
+            })
+        }
+        
+        // then
+        waitForExpectations(timeout: 20, handler: { _ in
+            let ctx = self.storage.mainThreadManagedObjectContext!
+            let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
+            XCTAssertEqual(sessions.count, 2)
+            
+            XCTAssertNotNil(sessions.first { (o) -> Bool in
+                return (o.startDate.timeIntervalSince1970 == now + 35) && (o.endDate.timeIntervalSince1970 == now + 40)
+            })
+            XCTAssertNotNil(sessions.first { (o) -> Bool in
+                return (o.startDate.timeIntervalSince1970 == now) && (o.endDate.timeIntervalSince1970 == now + 5)
+            })
+            
+        })
+    }
+    
+    func testSessionReportDoesNotHaveSessionDuplicates() {
+        MMTestCase.cleanUpAndStop()
+        MMTestCase.startWithCorrectApplicationCode()
+        weak var expectation = self.expectation(description: "case is finished")
+        
+        let remoteApiProvider = RemoteAPIProviderStub()
+        remoteApiProvider.sendUserSessionClosure = { _, _, _ in
+            return UserSessionSendingResult.Success(EmptyResponse())
+        }
+        mobileMessagingInstance.remoteApiProvider = remoteApiProvider
+        let now = MobileMessaging.date.now.timeIntervalSince1970
+        
+        // when
+        mobileMessagingInstance.pushRegistrationId = "reg1"
+        self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+            self.mobileMessagingInstance.pushRegistrationId = "reg2"
+            self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: false) {
+                
+                timeTravel(to: Date(timeIntervalSince1970: now + 35), block: {
+                    
+                    self.mobileMessagingInstance.userSessionService.performSessionTracking(doReporting: true) {
+                        expectation?.fulfill()
+                    }
+                })
+            }
+        }
+        
+        // then
+        waitForExpectations(timeout: 20, handler: { _ in
+            let ctx = self.storage.mainThreadManagedObjectContext!
+            let sessions = UserSessionReportObject.MM_findAllInContext(ctx)!
+            XCTAssertEqual(sessions.count, 1) // one unreported session remains
+        })
+    }
+    
+    func testDegradationUserSessionMapperCrashesWhenTwoSessionStartedWithinSecond() {
+        let d1 = Date()
+        let d2 = Date().addingTimeInterval(0.1)
+        let ctx = self.storage.mainThreadManagedObjectContext!
+        let s1 = UserSessionReportObject.MM_createEntityInContext(context: ctx)
+        s1.pushRegistrationId = "1"
+        s1.startReported = false
+        s1.startDate = d1
+        s1.endDate = d2
+        let s2 = UserSessionReportObject.MM_createEntityInContext(context: ctx)
+        s2.pushRegistrationId = "2"
+        s2.startReported = false
+        s2.startDate = d2
+        s2.endDate = d2
+        XCTAssertNotNil(UserSessionMapper.requestPayload(newSessions: [], finishedSessions: [s1, s2]))
+    }
 }
