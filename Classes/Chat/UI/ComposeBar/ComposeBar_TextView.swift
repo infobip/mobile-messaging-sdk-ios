bb619aa8a9068d149bf62fd85e80b57c93b43ed8
Francisco Fortes <Francisco.Fortes@infobip.com>
2022-07-13 07:41:57 +0000

    Pull request #211: CHAT-1129 ComposerBar textView contentOffset never updated when typing
    Merge in MML/infobip-mobile-messaging-ios from ffortes-CHAT-1129-contentOffset_multiline-bug to master
    
    Squashed commit of the following:
    
    commit f3144650b8b0f975264af8f90a1e2c737faa336d
    Author: Francisco Fortes <francisco.fortes@infobip.com>
    Date:   Fri Jul 8 13:12:49 2022 +0200
    
        CHAT-1129 ComposerBar textView contentOffset never updated when typing

--- Classes/Chat/UI/ComposeBar/ComposeBar_TextView.swift
+++ Classes/Chat/UI/ComposeBar/ComposeBar_TextView.swift
@@ -8,20 +8,11 @@
 import Foundation
 
 class ComposeBar_TextView: UITextView {
-	override var contentOffset: CGPoint {
-		set {
-			if selectedRange.length != 0 || isTracking || isDecelerating {
-				super.contentOffset = newValue
-			}
-		}
-		get {
-			return super.contentOffset
-		}
-	}
-	
 	var primitiveContentOffset: CGPoint {
 		set {
-			super.contentOffset = newValue
+            if selectedRange.length != 0 || isTracking || isDecelerating {
+                super.contentOffset = newValue
+            }
 		}
 		get {
 			return super.contentOffset
