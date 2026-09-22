a900859adcdf9a7bf27880940c2bf804085a917b
infobip-ci <infobip-ci@infobip.com>
2026-09-22 12:26:05 +0200

    Version bump for SDK: 15.7.6

--- Classes/MobileMessaging/Core/MMVersion.swift
+++ Classes/MobileMessaging/Core/MMVersion.swift
@@ -7,5 +7,5 @@
 //
 
 class MMVersion {
-    static let mobileMessagingVersion = "15.7.5"
+    static let mobileMessagingVersion = "15.7.6"
 }
--- Classes/NotificationExtension/MMNSEVersion.swift
+++ Classes/NotificationExtension/MMNSEVersion.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 enum MMNSEVersion {
-    static let mobileMessagingVersion = "15.7.5"
+    static let mobileMessagingVersion = "15.7.6"
 }
